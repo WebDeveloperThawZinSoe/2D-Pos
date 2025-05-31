22d72d2434aebcd12e0e56e05dd0ff9771dcb42e
Thaw Zin Soe <thawzinsoe.dev@gmail.com>
2025-05-31 10:29:03 +0630

    Dine Edit Fix

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\UserResource\Pages;
 use App\Filament\Resources\UserResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Support\Facades\Hash;
 
 class EditUser extends EditRecord
 {
@@ -15,6 +16,23 @@ class EditUser extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Get the new password from form state
+        $newPassword = $this->form->getState()['password'] ?? null;
+
+        if (!empty($newPassword)) {
+            $data['plain_password'] = $newPassword;
+            $data['password'] = Hash::make($newPassword);
+        } else {
+            // Keep existing password if no new password is provided
+            $data['password'] = $this->record->password;
+            $data['plain_password'] = $this->record->plain_password;
+        }
+
+        return $data;
     }
 
     protected function afterSave(): void
